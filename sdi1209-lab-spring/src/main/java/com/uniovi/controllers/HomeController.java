c3d31ea6b6401651f2ee007159a16d270e49fa86
pablorodriguez <valdes86@gmail.com>
2019-03-01 18:56:14 +0100

    SDI - 1.12 Página de inicio y 1.13 Diseño de plantillas con Thymeleaf

---
+++ sdi1209-lab-spring/src/main/java/com/uniovi/controllers/HomeController.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.uniovi.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Pablo Rodríguez Valdés
+ *
+ *         SDI - 2019
+ */
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+}
