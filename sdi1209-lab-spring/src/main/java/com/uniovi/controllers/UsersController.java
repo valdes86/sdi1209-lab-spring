67ffce922a4c910f1eb549e736a99c683c489f67
pablorodriguez <valdes86@gmail.com>
2019-03-04 10:42:15 +0100

    SDI-SpringBoot-6- Validación de datos en el servidor

--- sdi1209-lab-spring/src/main/java/com/uniovi/controllers/UsersController.java
+++ sdi1209-lab-spring/src/main/java/com/uniovi/controllers/UsersController.java
@@ -8,6 +8,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.uniovi.entities.User;
 import com.uniovi.services.SecurityService;
 import com.uniovi.services.UsersService;
+import com.uniovi.validators.SignUpFormValidator;
 
 /**
  * @author Pablo Rodríguez Valdés
@@ -29,6 +32,9 @@ public class UsersController {
 
 	@Autowired
 	private SecurityService securityService;
+
+	@Autowired
+	private SignUpFormValidator signUpFormValidator;
 
 	@RequestMapping("/user/list")
 	public String getListado(Model model) {
@@ -75,12 +81,17 @@ public class UsersController {
 	}
 
 	@RequestMapping(value = "/signup", method = RequestMethod.GET)
-	public String signup() {
+	public String signup(Model model) {
+		model.addAttribute("user", new User());
 		return "signup";
 	}
 
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
-	public String signup(@ModelAttribute("user") User user, Model model) {
+	public String signup(@Validated User user, BindingResult result) {
+		signUpFormValidator.validate(user, result);
+		if (result.hasErrors()) {
+			return "signup";
+		}
 		usersService.addUser(user);
 		securityService.autoLogin(user.getDni(), user.getPasswordConfirm());
 		return "redirect:home";
